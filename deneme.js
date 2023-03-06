3c17e3eb9d3408342b100f3710d84bd75d8cee96
Mustafa Koc <34mustafakoc@gmail.com>
2023-03-06 12:14:02 +0300

    new add function

--- deneme.js
+++ deneme.js
@@ -16,7 +16,7 @@ const cevapInput = document.getElementById("cevap-input");
 const myCar = document.getElementById("myCar");
 const soruCevapDOM = document.querySelector(".soru-cevap-main");
 const geriSayimDOM = document.querySelector("#geri-sayim-zaman");
-
+const timeStartInput = document.querySelector("#zaman-baslangic-input");
 
 let genislik = window.matchMedia("(max-width: 896px)")
 let carPositionLeft;
@@ -42,77 +42,84 @@ checkboxes.forEach(function (checkbox) {
                 checkbox.checked = false;
             });
             this.checked = true;
-            let randomNumber1;
-            let randomNumber2;
-            oyunBaslatBtn.addEventListener("click", baslatFunc)
-            function baslatFunc() {
-
-                if (Number(ilkSayi.value) == 1 && Number(ikinciSayi.value) == 1) {
-                    randomNumber1 = Math.floor(Math.random() * 9);
-                    randomNumber2 = Math.floor(Math.random() * 9);
-                }
-                else if (Number(ilkSayi.value) == 2 && Number(ikinciSayi.value) == 2) {
-                    randomNumber1 = Math.floor(Math.random() * 89 + 10);
-                    randomNumber2 = Math.floor(Math.random() * 89 + 10);
-                }
-                cevapInput.value = null;
-                num1.innerHTML = randomNumber1;
-                num2.innerHTML = randomNumber2;
-                let toplamSonuc;
-                if (toplaCheck.checked) {
-                    islemSembol.innerHTML = "+";
-                    toplamSonuc = randomNumber1 + randomNumber2;
-                } else if (cikarCheck.checked) {
-                    islemSembol.innerHTML = "-";
-                    toplamSonuc = randomNumber1 - randomNumber2;
-                } else if (carpCheck.checked) {
-                    islemSembol.innerHTML = "X";
-                    toplamSonuc = randomNumber1 * randomNumber2;
-                } else if (bolCheck.checked) {
-                    randomNumber2 = Math.floor(Math.random() * 8 + 1);
-                    islemSembol.innerHTML = "/";
-                    num2.innerHTML = randomNumber2;
-                    toplamSonuc = randomNumber1 / randomNumber2;
-                }
-                function keydownHandler(e) {
-                    if (e.key == "Enter") {
-                        if (toplamSonuc == Number(cevapInput.value)) {
-                            console.log("doğru cevap");
-                            carPositionLeft -= 30;
-                            carPositionBottom -= 20;
-                            myCar.style.left = `${carPositionLeft}px`
-                            myCar.style.bottom = `${carPositionBottom}px`
-                            if (carPositionLeft == 460 && carPositionBottom == 80) {
-                                soruCevapDOM.innerHTML = "Tebrikler..";
-                            } else if (carPositionLeft == 70 && carPositionBottom == 20) {
-                                soruCevapDOM.innerHTML = "Tebrikler..";
-                            }
-                        } else {
-                            console.log("yanlış cevap");
-                        }
-                        cevapInput.removeEventListener("keydown", keydownHandler);
-                        return baslatFunc();
-                    }
-                }
-                cevapInput.addEventListener("keydown", keydownHandler);
-                oyunSayfasi.style.display = "block";
-                girisSayfasi.style.display = "none";
-            }
-            let saniye = 20;
-            let geriSayim = setInterval(geriSayimFunc, 1000);
-            function geriSayimFunc() {
-                saniye--
-                geriSayimDOM.innerHTML = saniye;
-                if (saniye == 0) {
-                    inputBtnGrup.innerText = "Süre Doldu.."
-                    clearInterval(geriSayim)
-                }
-            }
         } else {
             this.checked = false;
         }
     });
 });
+
+let randomNumber1;
+let randomNumber2;
+oyunBaslatBtn.addEventListener("click", baslatFunc)
+function baslatFunc() {
+    if (Number(ilkSayi.value) == 1 && Number(ikinciSayi.value) == 1) {
+        randomNumber1 = Math.floor(Math.random() * 9);
+        randomNumber2 = Math.floor(Math.random() * 9);
+    }
+    else if (Number(ilkSayi.value) == 2 && Number(ikinciSayi.value) == 2) {
+        randomNumber1 = Math.floor(Math.random() * 89 + 10);
+        randomNumber2 = Math.floor(Math.random() * 89 + 10);
+    }
+    cevapInput.value = null;
+    num1.innerHTML = randomNumber1;
+    num2.innerHTML = randomNumber2;
+    let toplamSonuc;
+    if (toplaCheck.checked) {
+        islemSembol.innerHTML = "+";
+        toplamSonuc = randomNumber1 + randomNumber2;
+    } else if (cikarCheck.checked) {
+        islemSembol.innerHTML = "-";
+        toplamSonuc = randomNumber1 - randomNumber2;
+    } else if (carpCheck.checked) {
+        islemSembol.innerHTML = "X";
+        toplamSonuc = randomNumber1 * randomNumber2;
+    } else if (bolCheck.checked) {
+        randomNumber2 = Math.floor(Math.random() * 8 + 1);
+        islemSembol.innerHTML = "/";
+        num2.innerHTML = randomNumber2;
+        toplamSonuc = randomNumber1 / randomNumber2;
+    }
+    function keydownHandler(e) {
+        if (e.key == "Enter") {
+            if (toplamSonuc == Number(cevapInput.value)) {
+                console.log("doğru cevap");
+                carPositionLeft -= 30;
+                carPositionBottom -= 20;
+                myCar.style.left = `${carPositionLeft}px`
+                myCar.style.bottom = `${carPositionBottom}px`
+                if (carPositionLeft == 460 && carPositionBottom == 80) {
+                    soruCevapDOM.innerHTML = "Tebrikler..";
+                } else if (carPositionLeft == 70 && carPositionBottom == 20) {
+                    soruCevapDOM.innerHTML = "Tebrikler..";
+                }
+            } else {
+                console.log("yanlış cevap");
+            }
+            cevapInput.removeEventListener("keydown", keydownHandler);
+            return baslatFunc();
+        }
+    }
+    cevapInput.addEventListener("keydown", keydownHandler);
+    setTimeout(() => {
+        oyunSayfasi.style.display = "block";
+        girisSayfasi.style.display = "none";
+    }, 700)
+}
+
+oyunBaslatBtn.addEventListener("click", setIntervalFunc)
+function setIntervalFunc() {
+    console.log("butona tıklandı")
+    let saniyeGeriBaslangic = parseInt(timeStartInput.value);
+    let geriSayim = setInterval(geriSayimFunc, 1000);
+    function geriSayimFunc() {
+        saniyeGeriBaslangic--
+        geriSayimDOM.innerHTML = `${saniyeGeriBaslangic} sn`;
+        if (saniyeGeriBaslangic == 0) {
+            inputBtnGrup.innerText = "Süre Doldu.."
+            clearInterval(geriSayim)
+        }
+    }
+}
 
 geriDonBtn.addEventListener("click", () => {
     girisSayfasi.style.display = "flex";
